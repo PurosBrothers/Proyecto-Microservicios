3793ff2a89d71af726b41058d3ee2e944e04a960
ahenaon00 <ahenao.2005@gmail.com>
2025-09-24 16:26:17 -0500

    added testing configuration for message queues

--- marketplace-ms/src/test/java/com/microservicios/marketplace_ms/MarketplaceMsApplicationTests.java
+++ marketplace-ms/src/test/java/com/microservicios/marketplace_ms/MarketplaceMsApplicationTests.java
@@ -1,7 +1,11 @@
 package com.microservicios.marketplace_ms;
-
 import org.junit.jupiter.api.Test;
+import org.springframework.amqp.core.Queue;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Bean;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 
 @SpringBootTest
 class MarketplaceMsApplicationTests {
@@ -9,5 +13,14 @@ class MarketplaceMsApplicationTests {
 	@Test
 	void contextLoads() {
 	}
+
+	static final String topicExchangeName = "spring-boot-exchange";
+
+	static final String queueName = "spring-boot";
+
+	@Bean
+	Queue queue() {
+		return new Queue(queueName, false);
+  	}
 
 }
