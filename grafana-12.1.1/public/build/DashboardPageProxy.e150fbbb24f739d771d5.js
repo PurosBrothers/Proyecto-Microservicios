8be57c4eee2adc071a391fb0e44ef27577e40bc7
joshuaPZ <joshuaprieto8@gmail.com>
2025-09-25 22:40:58 -0500

    ADD: Grafana configurado con prometheus

---
+++ grafana-12.1.1/public/build/DashboardPageProxy.e150fbbb24f739d771d5.js
@@ -0,0 +1,3 @@
+"use strict";(self.webpackChunkgrafana=self.webpackChunkgrafana||[]).push([[9136],{20230:(a,s,e)=>{a.exports=e.p+"static/img/grab_dark.4ee8d28b.svg"},5878:(a,s,e)=>{a.exports=e.p+"static/img/grab_light.322c3334.svg"}}]);
+
+//# sourceMappingURL=DashboardPageProxy.e150fbbb24f739d771d5.js.map
