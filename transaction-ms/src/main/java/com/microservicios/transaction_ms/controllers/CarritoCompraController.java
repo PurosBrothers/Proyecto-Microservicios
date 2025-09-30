aa56d68bc2083d7a351fe577688c87b79c357476
joshuaPZ <joshuaprieto8@gmail.com>
2025-09-30 15:49:00 -0500

    add: funcionalidad completa de traer la lista de items en carrito

--- transaction-ms/src/main/java/com/microservicios/transaction_ms/controllers/CarritoCompraController.java
+++ transaction-ms/src/main/java/com/microservicios/transaction_ms/controllers/CarritoCompraController.java
@@ -16,6 +16,7 @@ import com.microservicios.transaction_ms.services.CarritoCompraService;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -26,7 +27,7 @@ public class CarritoCompraController {
     @Autowired
     private CarritoCompraService carritoCompraService;
 
-    @GetMapping("/list-items-carrito")
+    @PostMapping("/list-items-carrito")
     public ResponseEntity<?> getAllItems(@RequestBody UidRequestDTO request) {
         String uid = request.getUid();
         List<ItemCarritoDTO> items = carritoCompraService.getCarritoItems(uid).stream()
