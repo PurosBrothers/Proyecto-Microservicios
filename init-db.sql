9bde9c185db26e022e01720bc5c1dfb50f2a39c6
joshuaPZ <joshuaprieto8@gmail.com>
2025-11-25 22:50:43 -0500

    add: gateway y eureka dockerizadoss

---
+++ init-db.sql
@@ -0,0 +1,11 @@
+-- Crear bases de datos si no existen
+DO $$
+BEGIN
+   IF NOT EXISTS (SELECT FROM pg_database WHERE datname = 'marketplace_db') THEN
+      CREATE DATABASE marketplace_db;
+   END IF;
+   IF NOT EXISTS (SELECT FROM pg_database WHERE datname = 'keycloak_db') THEN
+      CREATE DATABASE keycloak_db;
+   END IF;
+END
+$$;
